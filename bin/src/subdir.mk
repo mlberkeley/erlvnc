8a16a188e14ee8f856cf2111729e3feb055adbe4
William Guss <carlostnb@gmail.com>
2016-03-06 01:52:10 -0800

    Finished work on ErlVNC initial build process. TODO: Start olinking extern lib vnc library.

--- bin/src/subdir.mk
+++ bin/src/subdir.mk
@@ -4,20 +4,20 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/erlvpn.cpp 
+../src/erlvnc.cpp 
 
 OBJS += \
-./src/erlvpn.o 
+./src/erlvnc.o 
 
 CPP_DEPS += \
-./src/erlvpn.d 
+./src/erlvnc.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	g++ -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -I/home/william/workspace/dev/lib/libvncserver -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- src/erlvnc.cpp
+++ src/erlvnc.cpp
@@ -8,10 +8,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <rfb/rfbclient.h>
+
 
 int main(void) {
 	puts("ERLVNC v1");
+	rfbClient* cl;
+	cl = rfbGetClient(8,3,4);
+
 	return EXIT_SUCCESS;
 }
-
 
